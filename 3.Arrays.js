2d9110c5de9e63ef39c012a1c6479ddb203d92fd
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-25 15:29:38 -0300

    Add Ex 3

---
+++ 3.Arrays.js
@@ -0,0 +1,29 @@
+// --- Arrays ---
+
+/* a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+mostrar por consola los meses 5 y 11 (utilizar console.log)*/
+//Solution:
+
+
+/* b) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+//Solution:
+
+
+/* c) Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+//Solution:
+
+
+/* d) Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+//Solution:
+
+
+/* e) Invertir el orden del array (utilizar reverse)*/
+//Solution:
+
+
+/* f) Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).*/
+//Solution:
+
+
+/* g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
+//Solution:
