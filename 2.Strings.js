24c051de52c392a4c889bc78d5a2c6cd20c0c492
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-25 15:29:24 -0300

    Add Ex 2

---
+++ 2.Strings.js
@@ -0,0 +1,35 @@
+// --- Strings ---
+
+/* a) Crear una variable de tipo string con al menos 10 caracteres y convertir
+todo el texto en mayúscula (utilizar toUpperCase).*/
+//Solution:
+
+
+
+
+/* b) Crear una variable de tipo string con al menos 10 caracteres y generar un
+nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+//Solution:
+
+
+/* c) Crear una variable de tipo string con al menos 10 caracteres y generar un
+nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+//Solution:
+
+
+/* d) Crear una variable de tipo string con al menos 10 caracteres y generar un
+nuevo string con la primera letra en mayúscula y las demás en minúscula.
+Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+//Solution:
+
+
+/* e) Crear una variable de tipo string con al menos 10 caracteres y algún espacio
+en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+//Solution:
+
+
+/* f) Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string
+que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula
+(utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
+//Solution:
