eaddd9b72e27a9b85913820782fe821b62c5e501
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-27 02:41:50 -0300

    Ex 6

--- 6.Funciones.js
+++ 6.Funciones.js
@@ -1,10 +1,17 @@
-// --- Funciones ---
-
+console.log(' ---6.Funciones ---');
 /* a) Crear una función suma que reciba dos valores numéricos y retorne el resultado.
 Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable
 en la consola del navegador.*/
 //Solution:
+var x,y,plus;
+x = 3;
+y = 4;
+plus = x + y;
 
+function suma(x,y) {
+    return{plus}
+};
+console.log('6.a) ' + plus);
 
 /* b) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros
 no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y
