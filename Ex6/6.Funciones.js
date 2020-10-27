297aa022d8d8b353e67831833ff9f5baa5de2e66
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-27 12:25:50 -0300

    Update Ex6

--- Ex6/6.Funciones.js
+++ Ex6/6.Funciones.js
@@ -24,8 +24,9 @@ x = '3';
 
 if ((typeof x !== 'number') || (typeof y !== 'number') ) {
     alert('ERROR: One of the parameters is Not-A-NUmber')
+} if ((Number.isInteger(x)) && (Number.isInteger(y)) ) {
+    //return 'They are wholes numbers'
 };
-
 console.log('b) ', plus(x,y));
 
 /* c) Crear una función validate integer que reciba un número como parámetro y verdadero si es un número entero.
@@ -35,7 +36,7 @@ Convertir la validación del ejercicio 6b) en una función separada y llamarla d
 probando que todo siga funcionando igual.*/
 //Solution:
 
-var alfa = 3;
+var alfa = 21;
 
 function valInt(alfa) {
     if (Number.isInteger(alfa)) {
