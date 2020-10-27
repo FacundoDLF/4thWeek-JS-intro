874ce3d0a9d3ad6b8a14d5a4d5de37cfb7e96d5b
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-27 11:20:23 -0300

    Update Ex6

--- Ex6/6.Funciones.js
+++ Ex6/6.Funciones.js
@@ -4,32 +4,27 @@ console.log(' ---6.Funciones ---');
 Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable
 en la consola del navegador.*/
 //Solution:
-var x,y,plus;
+var x,y;
 x = 3;
 y = 4;
-plus = x + y;
 
-function suma(x,y) {
-    return{plus}
+function plus(x,y) {
+    return[x + y]
 };
-console.log('6.a) ' + plus);
+console.log('a) ', plus(x,y));
 
 /* b) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros
 no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y
 retornar el valor NaN como resultado.*/
 //Solution:
 
-function suma(x,y) {
-    if ((x !== Number.NaN) || (y !== Number.NaN)) {
-        alert('ERROR: One of the parameters is Not-A-Number.');
-        return{Nan};
-    }
-    if (suma(x,y)){
-        return{plus}
-    };
-};
-console.log('6.b) Alert');
+x = '3';
 
+if ((x === Number.NaN) || (y === Number.NaN)) {
+    alert('ERROR: One of the parameters is Not-A-NUmber')
+} else {
+    console.log('b) ', plus(x,y))
+    };
 
 /* c) Crear una función validate integer que reciba un número como parámetro y verdadero si es un número entero.
 A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros.
@@ -37,3 +32,4 @@ En caso que haya decimales mostrar un alerta con el error y retorna el número c
 Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma
 probando que todo siga funcionando igual.*/
 //Solution:
+
