3ea42bcf1ca687296b9e4ef74af0ca69159e8112
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-27 02:17:38 -0300

    Ex 5

--- 5.For.js
+++ 5.For.js
@@ -3,7 +3,7 @@ console.log(' --- 5.For ---');
 /* a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript
 para mostrar una alerta utilizando cada una de las palabras.*/
 //Solution:
-var fingers = ['thumb', 'index', 'middle', 'ringFinger', 'pinky'];
+var fingers = ['thumb ', 'index ', 'middle ', 'ringFinger ', 'pinky '];
 
 for (var i = 0; i < 5; i++){
     console.log('5.a) Alert')
@@ -25,7 +25,13 @@ Al final mostrar una única alerta con la cadena completa.*/
 //Solution:
 var sentence = '';
 
-for (var i = 0; i < 5; i++);
+for (var i = 0; i < 5; i++){
+    if (sentence === fingers) {
+    }else {
+        sentence += fingers[i]
+    }/* alert('5.c) ' + sentence) --> From the inside adds one by one*/
+};
+alert('5.c) ' + sentence);
 
 /* d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
 es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array,
