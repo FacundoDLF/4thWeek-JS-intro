f66be84da9ab20da6e3170676f6b730fa00be1b6
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-27 02:10:24 -0300

    Ex 4

--- 4.IfElse.js
+++ 4.IfElse.js
@@ -1,4 +1,4 @@
-console.log(' --- If Else ---');
+console.log(' --- 4.If Else ---');
 
 /* a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(),
 si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5”
@@ -8,9 +8,9 @@ var numRandom = Math.random();
 console.log('a) ' + numRandom);
 
 if (numRandom >= 0.5) { // ---> It took to much time 'till I realise I should use period instead of coma.
-    alert('Greater than 0,5')
+    alert('4.a) Greater than 0,5')
 } else {
-    alert('Lower than 0,5')
+    alert('4.a) Lower than 0,5')
 };
 
 /* b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre
@@ -29,17 +29,17 @@ var age = 27;
 console.log('b) Age = ' + age);
 
 if (age <2) {
-    alert('Bebe')
+    alert('4.b) Bebe')
 } else if ((age >=2) && (age<=12)) {
-    alert('Niño')
+    alert('4.b) Niño')
 } else if ((age >=13) && (age<=19)) {
-    alert('Adolecente')
+    alert('4.b) Adolecente')
 } else if ((age >=20) && (age<=30)) {
-    alert('Joven')
+    alert('4.b) Joven')
 } else if ((age >=31) && (age<=60)) {
-    alert('Adulto')
+    alert('4.b) Adulto')
 } else if ((age >=61) && (age<=75)) {
-    alert('Adulto mayor')
+    alert('4.b) Adulto mayor')
 } else if (age > 75) {
-    alert('Anciano')
+    alert('4.b) Anciano')
 };
