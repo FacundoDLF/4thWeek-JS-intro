bc37a8ec2973dafd25e375afa15fe628102208e7
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-10-27 12:08:30 -0300

    Update Ex6

--- Ex6/6.Funciones.js
+++ Ex6/6.Funciones.js
@@ -9,9 +9,11 @@ x = 3;
 y = 4;
 
 function plus(x,y) {
-    return[x + y]
+    return[ x + y]
 };
-console.log('a) ', plus(x,y));
+
+var result = plus(x,y);
+console.log('a) ', result);
 
 /* b) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros
 no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y
@@ -20,11 +22,11 @@ retornar el valor NaN como resultado.*/
 
 x = '3';
 
-if ((x === Number.NaN) || (y === Number.NaN)) {
+if ((typeof x !== 'number') || (typeof y !== 'number') ) {
     alert('ERROR: One of the parameters is Not-A-NUmber')
-} else {
-    console.log('b) ', plus(x,y))
-    };
+};
+
+console.log('b) ', plus(x,y));
 
 /* c) Crear una función validate integer que reciba un número como parámetro y verdadero si es un número entero.
 A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros.
@@ -32,4 +34,15 @@ En caso que haya decimales mostrar un alerta con el error y retorna el número c
 Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma
 probando que todo siga funcionando igual.*/
 //Solution:
+
+var alfa = 3;
+
+function valInt(alfa) {
+    if (Number.isInteger(alfa)) {
+        return alfa + ' Its a whole number';
+    }else {
+        return alfa + ' Its not a whole number';
+    }
+};
+console.log('c) ' , valInt(alfa));
 
